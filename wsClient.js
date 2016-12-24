bc3792b2cef3e0540f9fa4686bd0f76a15df27f3
unknown <jianyeruan@gmail.com>
2016-12-24 01:11:17 -0500

    a

--- wsClient.js
+++ wsClient.js
@@ -1,22 +1,13 @@
 var W3CWebSocket = require('websocket').w3cwebsocket;
  var rest = require('restler');
-var client = new W3CWebSocket('ws://192.168.1.100:3000/', 'echo-protocol');
- 
-client.onerror = function() {
+var client = new W3CWebSocket('ws://service520.com:3333/', 'bossreport');
+ client.onerror = function() {
     console.log('Connection Error');
 };
- 
 client.onopen = function() {
     console.log('WebSocket Client Connected');
     client.send('{"register":"M01"}');
-   /* function sendNumber() {
-        if (client.readyState === client.OPEN) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            client.send(number.toString());
-            //setTimeout(sendNumber, 1000);
-        }
-    }
-    sendNumber();*/
+
 };
  
 client.onclose = function() {
